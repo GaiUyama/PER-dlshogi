eb747cc25f168ebba3089987e241c2d0bf2f2590
GaiUyama <107541106+GaiUyama@users.noreply.github.com>
2022-07-25 05:27:23 +0900

    Create make_priority.py

---
+++ selfplay/make_priority.py
@@ -0,0 +1,14 @@
+import math
+
+def make_priority(eval, game_result, color):
+    if color == BLACK:
+        if game_result == BLACK_WIN:
+            return math.fabs(1000-eval)
+        if game_result == WHITE_WIN:
+            return math.fabs(-1000-eval)
+    else:
+        if game_result == BLACK_WIN:
+            return math.fabs(-1000-eval)
+        if game_result == WHITE_WIN:
+            return math.fabs(1000-eval)
+    return math.fabs(eval)
